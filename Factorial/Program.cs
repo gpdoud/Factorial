2296d39afbf2a03ea37ccfc1bc0753922904adee
Greg doud <gpdoud@gmail.com>
2017-09-07 11:29:49 -0400

    Initial commit

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Factorial {
 	class Program {
+		int Factorial(int nbr) {
+			var factorial = 1;
+			for(var idx = 2; idx <= nbr; idx++) {
+				factorial *= idx;
+			}
+			return factorial;
+		}
+		void Run() {
+			for (var nbr = 2; nbr <= 10; nbr++) {
+				Console.WriteLine($"The factorial of {nbr} is {Factorial(nbr)}");
+			}
+		}
 		static void Main(string[] args) {
+			new Program().Run();
 		}
 	}
 }
